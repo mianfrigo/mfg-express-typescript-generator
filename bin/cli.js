2ab0c2af4e5ccd2a9c7798f25a5b74851b65da92
Miguel Frias <mianfrigo@gmail.com>
2021-09-10 13:02:03 +0200

    handle promises error & add comments

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@ const inquirer = require('inquirer');
 const questions = require('../lib/cli-question');
 const chalk = require('chalk');
 const { done, generateApp } = require('../lib/express-generator');
-const { handleArgs, getProjectPath, pathExist } = require('../lib/utilities');
+const { handleArgs, getProjectPath, pathExists } = require('../lib/utilities');
 
 const boxen = require('boxen');
 
@@ -13,36 +13,44 @@ const boxen = require('boxen');
   const args = process.argv.slice(2);
   handleArgs(args);
   let destination = getProjectPath(args[0]);
-  const exist = await pathExist(destination);
-  if (exist) {
-    console.error(
-      chalk.red(
-        'The select directory already exist. Please select another name for your project.'
-      )
-    );
-    return;
+  try {
+    const exist = await pathExists(destination);
+    if (exist) {
+      console.error(
+        chalk.red(
+          'The select directory already exist. Please select another name for your project.'
+        )
+      );
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit();
   }
   inquirer
     .prompt(questions)
-    .then((cliAnswers) => {
-      console.log('\n');
-      console.log(
-        boxen('Express typescript generator', {
-          borderColor: 'yellow',
-          borderStyle: 'classic',
-          align: 'left',
-        })
-      );
-
-      generateApp(destination, cliAnswers).then(() => {
+    .then(async (cliAnswers) => {
+      try {
+        console.log('\n');
+        console.log(
+          boxen('Express typescript generator', {
+            borderColor: 'yellow',
+            borderStyle: 'classic',
+            align: 'left',
+          })
+        );
+        await generateApp(destination, cliAnswers);
         done();
-      });
+      } catch (err) {
+        console.error(err);
+        process.exit();
+      }
     })
     .catch((err) => {
       if (err.isTtyError) {
         process.exit();
       } else {
-        console.log(err);
+        console.error(err);
       }
     });
 })();
